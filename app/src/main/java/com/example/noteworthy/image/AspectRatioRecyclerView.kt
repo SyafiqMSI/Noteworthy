5cd64decbc41677193999c0b1fa02bb91c344648
Zankimen <mayyas.md4@gmail.com>
2024-06-03 17:21:59 +0700

    aspectratio bug

--- app/src/main/java/com/example/noteworthy/image/AspectRatioRecyclerView.kt
+++ app/src/main/java/com/example/noteworthy/image/AspectRatioRecyclerView.kt
@@ -6,11 +6,5 @@ import androidx.recyclerview.widget.RecyclerView
 
 class AspectRatioRecyclerView(context: Context, attrs: AttributeSet) : RecyclerView(context, attrs) {
 
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        val height = (measuredWidth * 0.75).toInt()
-        if (height != measuredHeight) {
-            setMeasuredDimension(measuredWidth, height)
-        }
-    }
+    
 }
