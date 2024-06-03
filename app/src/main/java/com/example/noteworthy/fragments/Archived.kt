945cc06f84db6b11c8bbc38ed63e4ddc3fd98123
abdanrafii <abdanrafii8@gmail.com>
2024-06-03 17:12:59 +0700

    fix:archive

--- app/src/main/java/com/example/noteworthy/fragments/Archived.kt
+++ app/src/main/java/com/example/noteworthy/fragments/Archived.kt
@@ -2,3 +2,9 @@ package com.example.noteworthy.fragments
 
 import com.example.noteworthy.R
 
+class Archived : NoteworthyFragment() {
+
+    override fun getBackground() = R.drawable.archive
+
+    override fun getObservable() = model.archivedNotes
+}
