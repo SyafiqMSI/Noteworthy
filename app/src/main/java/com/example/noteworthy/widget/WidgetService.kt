3211d901d687f3c05a9e1c6f90f776d97fbe39e2
abdanrafii <abdanrafii8@gmail.com>
2024-06-03 17:09:30 +0700

    fix:  widgetservice

--- app/src/main/java/com/example/noteworthy/widget/WidgetService.kt
+++ app/src/main/java/com/example/noteworthy/widget/WidgetService.kt
@@ -4,3 +4,10 @@ import android.content.Intent
 import android.widget.RemoteViewsService
 import com.example.noteworthy.miscellaneous.Constants
 
+class WidgetService : RemoteViewsService() {
+
+    override fun onGetViewFactory(intent: Intent): RemoteViewsFactory {
+        val id = intent.getLongExtra(Constants.SelectedBaseNote, 0)
+        return WidgetFactory(application, id)
+    }
+}
