d70362f4f356935136611c4f13ca6b88341dee57
Zankimen <mayyas.md4@gmail.com>
2024-06-03 17:25:11 +0700

    fixevent

--- app/src/main/java/com/example/noteworthy/image/Event.kt
+++ app/src/main/java/com/example/noteworthy/image/Event.kt
@@ -4,5 +4,10 @@ class Event<T>(val data: T) {
 
     private var isHandled = false
 
-    
+    fun handle(function: (data: T) -> Unit) {
+        if (!isHandled) {
+            function(data)
+            isHandled = true
+        }
+    }
 }
