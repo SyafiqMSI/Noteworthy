ad637976bcaffff7bdcadf146cda6cb99808aed1
abdanrafii <abdanrafii8@gmail.com>
2024-06-03 17:14:06 +0700

    fix delete

--- app/src/main/java/com/example/noteworthy/fragments/Deleted.kt
+++ app/src/main/java/com/example/noteworthy/fragments/Deleted.kt
@@ -6,3 +6,23 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.example.noteworthy.R
 import com.example.noteworthy.miscellaneous.add
 
+class Deleted : NoteworthyFragment() {
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.add(R.string.delete_all, R.drawable.delete_all) { deleteAllNotes() }
+    }
+
+
+    private fun deleteAllNotes() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setMessage(R.string.delete_all_notes)
+            .setPositiveButton(R.string.delete) { _, _ -> model.deleteAllBaseNotes() }
+            .setNegativeButton(R.string.cancel, null)
+            .show()
+    }
+
+
+    override fun getBackground() = R.drawable.delete
+
+    override fun getObservable() = model.deletedNotes
+}
